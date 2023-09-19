cd55d7ab954447ba4a19b9393f2088653ae1b360
HalleysAsteroid <95715834+HalleysAsteroid@users.noreply.github.com>
2023-09-18 20:33:08 -0400

    Week 2

---
+++ Week2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+double price = 18;
+const double SALES_TAX = 6.234;
+
+double total = price * (1 + (SALES_TAX / 100));
+Console.WriteLine("Your subtotal is {0}, and your total is {1}.", price.ToString("C"), total.ToString("C"));
+
+double five = 32_______________________________73;
+Console.WriteLine(five);
