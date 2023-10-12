d9d8c82f29502df0ac4a44e6aba3734d942cc1f5
HalleysAsteroid <95715834+HalleysAsteroid@users.noreply.github.com>
2023-10-12 00:28:40 -0400

    Lab3 Complete

--- Lab3_CreditCardValidator/CreditCardValidator.Designer.cs
+++ Lab3_CreditCardValidator/CreditCardValidator.Designer.cs
@@ -28,34 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            numKey = new NumericUpDown();
+            numKey = new MyNumericUpDown();
             lblSlogan = new Label();
             imgLogo = new PictureBox();
             panel1 = new Panel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            btnValidator = new Button();
+            btnRepayment = new Button();
+            btnPayday = new Button();
             btnForgiveness = new Button();
             imgCard = new PictureBox();
             lblNumbers = new Label();
             lblExpiration = new Label();
             lblGoodThru = new Label();
             lblName = new Label();
+            lblInstructions = new Label();
+            lblFiller = new Label();
+            lblValid = new Label();
+            btnSubmit = new Button();
+            label1 = new Label();
+            cmbType = new ComboBox();
+            panel2 = new Panel();
+            lblCardType = new Label();
             ((System.ComponentModel.ISupportInitialize)numKey).BeginInit();
             ((System.ComponentModel.ISupportInitialize)imgLogo).BeginInit();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)imgCard).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // numKey
             // 
             numKey.Font = new Font("OCR A Extended", 22F, FontStyle.Regular, GraphicsUnit.Point);
-            numKey.Location = new Point(395, 341);
+            numKey.Location = new Point(495, 433);
             numKey.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
             numKey.Name = "numKey";
             numKey.Size = new Size(120, 38);
             numKey.TabIndex = 0;
-            numKey.Leave += numKey_Leave;
             // 
             // lblSlogan
             // 
@@ -66,7 +74,6 @@
             lblSlogan.TabIndex = 1;
             lblSlogan.Text = "With you all the way.™";
             lblSlogan.TextAlign = ContentAlignment.MiddleCenter;
-            lblSlogan.Click += lblSlogan_Click;
             // 
             // imgLogo
             // 
@@ -87,34 +94,34 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 93);
             panel1.TabIndex = 5;
-            panel1.Paint += panel1_Paint;
             // 
-            // button1
+            // btnValidator
             // 
-            button1.Location = new Point(0, 94);
-            button1.Name = "button1";
-            button1.Size = new Size(200, 75);
-            button1.TabIndex = 6;
-            button1.Text = "Credit Card Validator";
-            button1.UseVisualStyleBackColor = true;
+            btnValidator.Location = new Point(0, 94);
+            btnValidator.Name = "btnValidator";
+            btnValidator.Size = new Size(200, 75);
+            btnValidator.TabIndex = 6;
+            btnValidator.Text = "Credit Card Validator";
+            btnValidator.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnRepayment
             // 
-            button2.Location = new Point(200, 94);
-            button2.Name = "button2";
-            button2.Size = new Size(200, 75);
-            button2.TabIndex = 7;
-            button2.Text = "Loan Repayment";
-            button2.UseVisualStyleBackColor = true;
+            btnRepayment.Location = new Point(200, 94);
+            btnRepayment.Name = "btnRepayment";
+            btnRepayment.Size = new Size(200, 75);
+            btnRepayment.TabIndex = 7;
+            btnRepayment.Text = "Loan Repayment";
+            btnRepayment.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnPayday
             // 
-            button3.Location = new Point(400, 94);
-            button3.Name = "button3";
-            button3.Size = new Size(200, 75);
-            button3.TabIndex = 8;
-            button3.Text = "Payday Loans";
-            button3.UseVisualStyleBackColor = true;
+            btnPayday.Location = new Point(400, 94);
+            btnPayday.Name = "btnPayday";
+            btnPayday.Size = new Size(200, 75);
+            btnPayday.TabIndex = 8;
+            btnPayday.Text = "Payday Loans";
+            btnPayday.UseVisualStyleBackColor = true;
+            btnPayday.Click += btnPayday_Click;
             // 
             // btnForgiveness
             // 
@@ -129,7 +136,7 @@
             // imgCard
             // 
             imgCard.Image = Properties.Resources.creditCardTemplate;
-            imgCard.Location = new Point(0, 175);
+            imgCard.Location = new Point(100, 267);
             imgCard.Name = "imgCard";
             imgCard.Size = new Size(600, 368);
             imgCard.SizeMode = PictureBoxSizeMode.Zoom;
@@ -141,7 +148,7 @@
             lblNumbers.AutoSize = true;
             lblNumbers.BackColor = Color.Transparent;
             lblNumbers.Font = new Font("OCR A Extended", 22F, FontStyle.Regular, GraphicsUnit.Point);
-            lblNumbers.Location = new Point(123, 343);
+            lblNumbers.Location = new Point(223, 435);
             lblNumbers.Name = "lblNumbers";
             lblNumbers.Size = new Size(266, 32);
             lblNumbers.TabIndex = 17;
@@ -152,7 +159,7 @@
             lblExpiration.AutoSize = true;
             lblExpiration.BackColor = Color.Transparent;
             lblExpiration.Font = new Font("OCR A Extended", 22F, FontStyle.Regular, GraphicsUnit.Point);
-            lblExpiration.Location = new Point(272, 439);
+            lblExpiration.Location = new Point(372, 531);
             lblExpiration.Name = "lblExpiration";
             lblExpiration.Size = new Size(104, 32);
             lblExpiration.TabIndex = 15;
@@ -162,7 +169,7 @@
             // 
             lblGoodThru.BackColor = Color.Transparent;
             lblGoodThru.Font = new Font("OCR A Extended", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            lblGoodThru.Location = new Point(229, 439);
+            lblGoodThru.Location = new Point(329, 531);
             lblGoodThru.Name = "lblGoodThru";
             lblGoodThru.Size = new Size(43, 32);
             lblGoodThru.TabIndex = 16;
@@ -174,25 +181,116 @@
             lblName.AutoSize = true;
             lblName.BackColor = Color.Transparent;
             lblName.Font = new Font("OCR A Extended", 22F, FontStyle.Regular, GraphicsUnit.Point);
-            lblName.Location = new Point(48, 471);
+            lblName.Location = new Point(148, 563);
             lblName.Name = "lblName";
             lblName.Size = new Size(266, 32);
             lblName.TabIndex = 14;
             lblName.Text = "Your Name Here";
             // 
+            // lblInstructions
+            // 
+            lblInstructions.Location = new Point(100, 643);
+            lblInstructions.Name = "lblInstructions";
+            lblInstructions.Size = new Size(600, 49);
+            lblInstructions.TabIndex = 18;
+            lblInstructions.Text = "Please enter the last four digits of the card you would like to validate in the box on the template above.";
+            lblInstructions.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblFiller
+            // 
+            lblFiller.AutoSize = true;
+            lblFiller.Location = new Point(204, 821);
+            lblFiller.Margin = new Padding(3, 0, 0, 0);
+            lblFiller.Name = "lblFiller";
+            lblFiller.Size = new Size(324, 23);
+            lblFiller.TabIndex = 19;
+            lblFiller.Text = "The card number you have entered is";
+            lblFiller.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblValid
+            // 
+            lblValid.AutoSize = true;
+            lblValid.BackColor = Color.Transparent;
+            lblValid.Location = new Point(523, 821);
+            lblValid.Margin = new Padding(0);
+            lblValid.Name = "lblValid";
+            lblValid.Size = new Size(73, 23);
+            lblValid.TabIndex = 20;
+            lblValid.Text = "invalid.";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.BackColor = Color.DeepSkyBlue;
+            btnSubmit.FlatAppearance.BorderColor = SystemColors.ControlText;
+            btnSubmit.FlatAppearance.MouseOverBackColor = Color.SkyBlue;
+            btnSubmit.FlatStyle = FlatStyle.Flat;
+            btnSubmit.ForeColor = SystemColors.ControlText;
+            btnSubmit.Location = new Point(345, 763);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(110, 50);
+            btnSubmit.TabIndex = 21;
+            btnSubmit.Text = "Check it!";
+            btnSubmit.UseVisualStyleBackColor = false;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Georgia", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(0, 173);
+            label1.Name = "label1";
+            label1.Size = new Size(800, 91);
+            label1.TabIndex = 22;
+            label1.Text = "Credit Card Validator";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // cmbType
+            // 
+            cmbType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbType.FormattingEnabled = true;
+            cmbType.Items.AddRange(new object[] { "Standard Card - 1", "Cash Back Card - 2", "Travel Card - 3", "Rewards Card - 4", "Student Account Card - 5", "Business Account Card - 6", "Store Partner Card - 7", "Employee Card - 8", "Temporary Gift Card - 9" });
+            cmbType.Location = new Point(330, 12);
+            cmbType.Name = "cmbType";
+            cmbType.Size = new Size(250, 31);
+            cmbType.TabIndex = 23;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(lblCardType);
+            panel2.Controls.Add(cmbType);
+            panel2.Location = new Point(0, 700);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 55);
+            panel2.TabIndex = 24;
+            // 
+            // lblCardType
+            // 
+            lblCardType.Location = new Point(220, 15);
+            lblCardType.Name = "lblCardType";
+            lblCardType.Size = new Size(104, 23);
+            lblCardType.TabIndex = 24;
+            lblCardType.Text = "Card Type:";
+            lblCardType.TextAlign = ContentAlignment.TopRight;
+            // 
             // CreditCardValidator
             // 
+            AcceptButton = btnSubmit;
             AutoScaleDimensions = new SizeF(11F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 877);
+            Controls.Add(panel2);
+            Controls.Add(label1);
+            Controls.Add(btnSubmit);
+            Controls.Add(lblValid);
+            Controls.Add(lblFiller);
+            Controls.Add(lblInstructions);
             Controls.Add(lblNumbers);
             Controls.Add(lblExpiration);
             Controls.Add(lblGoodThru);
             Controls.Add(lblName);
             Controls.Add(btnForgiveness);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(btnPayday);
+            Controls.Add(btnRepayment);
+            Controls.Add(btnValidator);
             Controls.Add(panel1);
             Controls.Add(numKey);
             Controls.Add(imgCard);
@@ -201,29 +299,36 @@
             Margin = new Padding(5);
             Name = "CreditCardValidator";
             Text = "Harvest & Trustee - Credit Card Validator";
-            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)numKey).EndInit();
             ((System.ComponentModel.ISupportInitialize)imgLogo).EndInit();
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)imgCard).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private NumericUpDown numKey;
         private Label lblSlogan;
         private PictureBox imgLogo;
         private Panel panel1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
+        private Button btnValidator;
+        private Button btnRepayment;
+        private Button btnPayday;
         private Button btnForgiveness;
         private PictureBox imgCard;
         private Label lblNumbers;
         private Label lblExpiration;
         private Label lblGoodThru;
         private Label lblName;
+        private Label lblInstructions;
+        private MyNumericUpDown numKey;
+        private Label lblFiller;
+        private Label lblValid;
+        private Button btnSubmit;
+        private Label label1;
+        private ComboBox cmbType;
+        private Panel panel2;
+        private Label lblCardType;
     }
 }
--- Lab3_CreditCardValidator/CreditCardValidator.cs
+++ Lab3_CreditCardValidator/CreditCardValidator.cs
@@ -18,35 +18,58 @@ namespace Lab3_CreditCardValidator
             lblExpiration.Location = new Point(272, 264);
             imgCard.Controls.Add(numKey);
             numKey.Location = new Point(395, 166);
+            cmbType.SelectedIndex = 0;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public class MyNumericUpDown : NumericUpDown
         {
+            protected override void UpdateEditText()
+            {
+                this.Text = this.Value.ToString().PadLeft(4, '0');
+            }
         }
 
-        private void numKey_Leave(object sender, EventArgs e)
+        public void calculateValidity(int digits, int divisor)
         {
-
+            String s = digits.ToString().PadLeft(4, '0');
+            int x = int.Parse(s.Substring(0, 3));
+            int y = int.Parse(s.Substring(3, 1));
+            if (isValidCard(x, y, divisor))
+            {
+                lblValid.ForeColor = Color.Green;
+                lblValid.Text = "valid!";
+            }
+            else
+            {
+                lblValid.ForeColor = Color.Red;
+                lblValid.Text = "invalid.";
+            }
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        public bool isValidCard(int leading, int quotient, int checkDigit)
         {
-
+            if (leading % checkDigit == quotient)
+            {
+                return true;
+            }
+            else return false;
         }
 
-        private void lblSlogan_Click(object sender, EventArgs e)
+        private void btnSubmit_Click(object sender, EventArgs e)
         {
-
+            int checkDigit = 1 + cmbType.SelectedIndex;
+            calculateValidity((int)numKey.Value, checkDigit);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void btnPayday_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
+            MessageBox.Show
+    (
+    "Unfortunately, due to a string of robberies in our Washington D.C. branches, we are unable to offer Payday Loans at this time. We thank you for your patronage and your continued patience.",
+    "Page Not Available",
+    MessageBoxButtons.OK,
+    MessageBoxIcon.Warning
+    );
         }
 
         private void btnForgiveness_Click(object sender, EventArgs e)
